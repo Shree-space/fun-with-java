9635198c6ae10730d92842b04d4a6680b4d549a7
Shree-space <52987470+Shree-space@users.noreply.github.com>
2019-08-12 18:15:47 +0530

    Update FindPalindrome.java
    using parametrized constructor

--- JavaAssignment/assignment5/FindPalindrome.java
+++ JavaAssignment/assignment5/FindPalindrome.java
@@ -6,11 +6,9 @@ public class FindPalindrome {
 
 	public static void main(String[] args) {
 		System.out.println("Please enter a string to test if a 'Palindrome'");
-			Scanner sc = new Scanner(System.in);
-			String inputString = sc.nextLine();
-			PalindromeChecker palindromechecker = new PalindromeChecker();
-			palindromechecker.setInputStr(inputString);
-		    System.out.println(palindromechecker.checkPalindrome(inputString)? "It's a Palindrome!": "It's not a Palindorme :-(");
+		Scanner sc = new Scanner(System.in);
+		PalindromeChecker palindromechecker = new PalindromeChecker(sc.nextLine());
+		System.out.println(palindromechecker.isPalindrome() ? "It's a Palindrome" : "Not a Palindrome :-(");
 
 }
 
